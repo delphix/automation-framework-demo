cd70118498e2e94284da24ffe1d51b911161c30d
Derek Smart <derek.smart@delphix.com>
2018-08-21 15:11:03 -0400

    enable cors on api
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/java/com/delphix/daf/repository/UserRepository.java
+++ src/main/java/com/delphix/daf/repository/UserRepository.java
@@ -3,8 +3,9 @@ package com.delphix.daf.repository;
 import java.util.List;
 import com.delphix.daf.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByUsername(String username);
