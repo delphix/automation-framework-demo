a984a163a39efe9f7d8ead59c8e0771352a72d9f
Derek Smart <derek.smart@delphix.com>
2018-08-22 17:13:41 -0400

    switch angular to port 80
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/java/com/delphix/daf/DafApplication.java
+++ src/main/java/com/delphix/daf/DafApplication.java
@@ -17,7 +17,7 @@ public class DafApplication implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
                 .allowCredentials(true);
     }
--- src/main/java/com/delphix/daf/repository/UserRepository.java
+++ src/main/java/com/delphix/daf/repository/UserRepository.java
@@ -3,9 +3,7 @@ package com.delphix.daf.repository;
 import java.util.List;
 import com.delphix.daf.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findByUsername(String username);
