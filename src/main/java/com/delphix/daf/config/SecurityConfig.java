e023fa4f982c7f6f71d2880ba33651402e397bb6
Derek Smart <derek.smart@delphix.com>
2018-08-28 15:55:52 -0400

    api calls to protected endpoints with JWT
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/java/com/delphix/daf/config/SecurityConfig.java
+++ src/main/java/com/delphix/daf/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.delphix.daf.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.delphix.daf.security.JwtEntryPoint;
+import com.delphix.daf.security.JwtAuthFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,9 +16,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+    @Autowired
+    private JwtEntryPoint unauthorizedHandler;
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().anyRequest().permitAll();
-        http.csrf().disable();
+        http
+            .csrf().disable()
+            .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth/sign-up").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
